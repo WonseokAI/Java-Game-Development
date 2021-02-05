d6b1ae47df434779a79b30188f7cd1330d5e5936
WonseokAI <ws4423@gmail.com>
2021-02-05 16:02:39 +0900

    javafx practice

--- Practice/javafx2/src/sample/Main.java
+++ Practice/javafx2/src/sample/Main.java
@@ -24,35 +24,39 @@ public class Main extends Application {
         //grid.setVgap(10);                       // label 사이에 세로 공간 넣기
         //grid.setAlignment(Pos.CENTER);
 
-//        for (int y = 0; y < 10; y++){
-//            for (int x = 0; x < 10; x++){
-//                Label label = new Label();
-//                label.setStyle("-fx-border-color: black; -fx-font-size: 15");
-//                label.setText(" ("+x+","+y+") ");
-//                grid.addRow(y, label);
-//            }
-//        }
+        for (int y = 0; y < 10; y++){
+            for (int x = 0; x < 10; x++){
+                Label label = new Label();
+                label.setStyle("-fx-border-color: black; -fx-font-size: 15");
+                label.setText(" ("+x+","+y+") ");
+                label.prefWidthProperty().bind(grid.widthProperty());
+                label.prefHeightProperty().bind(grid.heightProperty());
+                label.setAlignment(Pos.CENTER);
+                grid.addRow(y, label);
+            }
+        }
 
-        Label label1 = new Label("A");
-        Label label2 = new Label("B");
-        Label label3 = new Label("C");
-
-        label1.setStyle("-fx-border-color: black; -fx-font-size: 30");
-        label2.setStyle("-fx-border-color: black; -fx-font-size: 30");
-        label3.setStyle("-fx-border-color: black; -fx-font-size: 30");
-
-        grid.add(label1, 0, 0, 1, 1); // span은 얼마나 차지하느냐를 나타냄
-        grid.add(label2, 0, 1, 1, 1);
-        grid.add(label3, 1, 1, 1, 1);
+//        Label label1 = new Label("A");
+//        Label label2 = new Label("B");
+//        Label label3 = new Label("C");
+//
+//        label1.setStyle("-fx-border-color: black; -fx-font-size: 30");
+//        label2.setStyle("-fx-border-color: black; -fx-font-size: 30");
+//        label3.setStyle("-fx-border-color: black; -fx-font-size: 30");
+//
+//        grid.add(label1, 0, 0, 1, 1); // span은 얼마나 차지하느냐를 나타냄
+//        grid.add(label2, 0, 1, 1, 1);
+//        grid.add(label3, 1, 1, 1, 1);
 
         grid.setAlignment(Pos.CENTER);
+        grid.setStyle("-fx-border-color: blue;");
 
         stage.setScene(scene);
         stage.show();
     }
 
     GridPane grid = new GridPane();
-    Scene scene = new Scene(grid, 400, 400);
+    Scene scene = new Scene(grid, 600, 400);
 
     public static void main(String[] args) {
 
--- Practice/javafx3/src/sample/Main.java
+++ Practice/javafx3/src/sample/Main.java
@@ -4,20 +4,51 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        // stage > scene > container > node
+
+        // button 크기와 container 크기 자동 조절하기
+        Button button1 = new Button();
+        button1.setText("Button");
+        button1.prefWidthProperty().bind(pane.widthProperty());   // pane의 가로 길이와 button1의 가로 길이를 일치 시켰다.
+        button1.prefHeightProperty().bind(pane.heightProperty()); // pane의 세로 길이와 button1의 세로 길이를 일치 시켰다.
+
+        Button button2 = new Button();
+        button2.setText("Test");
+        // button의 최소 사이즈 정하기
+//        button2.setMinWidth(50);
+//        button2.setMinHeight(100);
+//        button2.setMinSize(50, 100);
+        // button의 최대 사이즈 정하기
+//        button2.setMaxWidth(50);
+//        button2.setMaxHeight(100);
+//        button2.setMaxSize(50, 100);
+        button2.prefWidthProperty().bind(pane.widthProperty());
+        button2.prefHeightProperty().bind(pane.heightProperty());
+
+        grid.addRow(0, button1, button2);
+
+        pane.getChildren().add(grid);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    GridPane grid = new GridPane();
+    StackPane pane = new StackPane();
+    Scene scene = new Scene(pane, 100, 100);
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
